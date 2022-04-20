7db2f40f91c5c29e27cbd80a4939f0b65f844e1d
Osman Forhad <osmanforhad93@gmail.com>
2022-04-20 10:18:26 +0600

    setup recycler view inside main activity and create linearlayout for the recycler view

--- app/src/main/java/developer/osmanforhad/recyclercardview/MainActivity.java
+++ app/src/main/java/developer/osmanforhad/recyclercardview/MainActivity.java
@@ -1,6 +1,8 @@
 package developer.osmanforhad.recyclercardview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //find Recycler View or connect XML RecyclerView View with Main Activity
+        RecyclerView RV = (RecyclerView) findViewById(R.id.recyclerViewId);
+        //create LinearLayout instance for RecyclerView
+        RV.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
