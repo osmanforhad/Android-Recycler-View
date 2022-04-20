8eb46f99255f6132fd68c65e0235d0f1392b5b8c
Osman Forhad <osmanforhad93@gmail.com>
2022-04-20 09:41:02 +0600

    setup adapter class

--- app/src/main/java/developer/osmanforhad/recyclercardview/MyAdaptar.java
+++ app/src/main/java/developer/osmanforhad/recyclercardview/MyAdaptar.java
@@ -1,4 +1,29 @@
 package developer.osmanforhad.recyclercardview;
 
-public class MyAdaptar {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdaptar extends RecyclerView.Adapter<MyViewHolder> {
+
+    public MyAdaptar() {
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
 }
